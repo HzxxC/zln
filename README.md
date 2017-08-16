e4f7effdcffd0c074a00d3c3a13c903667fb4ec8
Pengchu <3207603685@qq.com>
2017-08-16 17:37:45 +0800

    添加系统框架，修改列表页面，及文章页面样式，修改后台部分功能

---
+++ README.md
@@ -0,0 +1,12 @@
+全国中老年网站 v1.0.0
+
+前端+后台框架
+
+语言：PHP
+框架：ThinkPHP3.2.2
+数据库：MySQL
+
+本地测试网址：http://localhost/zln
+后台登录
+帐号：admin
+密码：asdasd
---
+++ application/Admin/Lang/zh-cn.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+		'ADMIN_CENTER' => '后台管理中心',
+		'WELCOME_USER' => '欢迎, {$username}',
+		'WEBSITE_HOME_PAGE' => '网站首页',
+		'WEB_NAME' => '全国中老年网'
+);
